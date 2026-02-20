bc5d1a2fe40262f55138e054a066213800567728
Aviles, Adriana (CW) <between-services.adriana.aviles@hp.com>
2026-02-20 08:14:56 +0100

    data exploration

--- get_data/credentials.py
+++
@@ -1,6 +0,0 @@
-# Credenciales para garage61.net
-# Rellena con tu email y contraseña de garage61.net
-# Si dejas los valores vacíos el script te los pedirá interactivamente
-
-GARAGE61_EMAIL = ""       # Tu email de garage61.net
-GARAGE61_PASSWORD = ""    # Tu contraseña de garage61.net
